5ddb9fd782ecb5485c309b79fe4aa4ebc6dc3262
chiharukakimoto <qianchun@hotmail.co.jp>
2021-10-13 22:50:24 +0900

    テーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :email, unique:true
-  validates :encrypted_password, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_day, presence: true
 end
