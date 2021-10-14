68b64683a9441cfd18fa3c9605c7969951d0e958
chiharukakimoto <qianchun@hotmail.co.jp>
2021-10-14 23:01:00 +0900

    ログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
   
   private
@@ -9,7 +10,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :email, presence: true, presence: true
-  validates :encrypted_password, presence: true
   validates :nickname, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
