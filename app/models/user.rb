377d4f9d62ff88aedcb0eb5b8992af0c4739f7c7
chiharukakimoto <qianchun@hotmail.co.jp>
2021-10-16 17:03:04 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_day, presence: true
+  with options presence true do
+  validates :nickname
+  validates :encrypted_password, length:{minimum:6},format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  validates :family_name, format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :first_name, format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :family_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :birth_day
 end
