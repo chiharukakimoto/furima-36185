994aa6d83259c948c1dcfb903607cd9300019f84
chiharukakimoto <qianchun@hotmail.co.jp>
2021-10-19 21:59:11 +0900

    アクティブハッシュ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
+    @articles = Article.order("created_at DESC")
+  end
+  
+  def new
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one :order
   has_one_attached :image
+  belongs_to :category
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :item_info, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true, format: {with: /^[0-9]+$/}
+  validates_inclusion_of :price, in:300..999999999
 end
