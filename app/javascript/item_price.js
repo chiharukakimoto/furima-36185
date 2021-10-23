3dd6a6e08e64f344c63e60aacc4346b966222707
chiharukakimoto <qianchun@hotmail.co.jp>
2021-10-24 00:10:51 +0900

    計算

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,13 +2,12 @@ window.addEventListener('load', () => {
   console.log("OK");
 });
 
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
-  console.log(inputValue);
-  const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML = priceInput.value * 0.1;
-  const profitDom = document.getElementById("profit");
-  profitDom.innerHTML = priceInput.value - priceInput.value * 0.1;
-})
-
+const priceInput = document.getElementById("item-price")
+priceInput.addEventListener("input", () => {
+   const inputValue = priceInput.value;
+   console.log(inputValue);
+   const addTaxDom = document.getElementById("add-tax-price")
+    addTaxDom.innerHTML = priceInput.value * 0.1;
+    const profitDom = document.getElementById("profit")
+    profitDom.innerHTML = priceInput.value * 0.9;
+});
