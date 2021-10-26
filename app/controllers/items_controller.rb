faba1504a0283e129e2475ecb247ba77f971c55c
chiharukakimoto <qianchun@hotmail.co.jp>
2021-10-26 21:35:32 +0900

    訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :update]
+  before_action :set_item, only: [:edit, :update, :show, ]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :without_index, only: [:edit, :update]
   def index
@@ -20,15 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.update(item_params)
       redirect_to item_path(@item.id)
