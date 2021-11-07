4eea204728dbaa6134a22116ffa1e81dd7cf99cf
chiharukakimoto <qianchun@hotmail.co.jp>
2021-11-07 22:35:40 +0900

    soldout

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
   
   def index
     @items = Item.all.order("created_at DESC")
+    @order_address = OrderAddress.new
   end
 
   def new
@@ -21,6 +22,7 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @order_address = OrderAddress.new
   end
 
   def edit
